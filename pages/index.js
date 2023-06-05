dde0f1cc284eada354d40a7bcc0cf2df47075a2f
VeniciaLovesYou <135560111+VeniciaLovesYou@users.noreply.github.com>
2023-06-05 02:44:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Head from 'next/head'
+const vidRef=useRef();
+
 
 const Home = (props) => {
+  
+  useEffect(() => { vidRef.current.play(); },[]);
   return (
     <>
       <div className="home-container">
@@ -12,6 +16,7 @@ const Home = (props) => {
         <video
           src="https://d2ts3zms369ah.cloudfront.net/zo6r%2Ffile%2F3d22dd743e183ee46e9e2cfda62d9414_video.mp4?response-content-disposition=inline%3Bfilename%3D%223d22dd743e183ee46e9e2cfda62d9414_video.mp4%22%3B&amp;response-content-type=video%2Fmp4&amp;Expires=1685944861&amp;Signature=A7zOQuyGFaRdgnjIgm4BoODDawhJTBYcyJCA3bz8znivXKfeEdj5W3Mx58JzT7SqNlp9vssRo3ZPrhDFEhSMNdpuk-Y2SBimKeS12SPYb3LCE~BOqWcQSdHk68BHOKcINZI5yjb3G3lQx4YXqCeBVNYWxpQQR~acl8l167ziK2cZLItYJvaVH2CK68WDh45lT8PPRXY30bhwrMMlurpW4EadPgMFW9ih-t-QZhY7-Fl7Jsp1JvCVrEUIHTDEvHWvNeSohDrPDzweUb6RoTPZ6eOsJaGUOViXGB96q7yyfzNfXVL3znZStfKBwRVHcbar1Is7a6MGxFtxwoZ63PLc5Q__&amp;Key-Pair-Id=APKAJT5WQLLEOADKLHBQ"
           loop   
+          ref={ vidRef }
           preload="none"
           autoPlay
           className="home-video"
